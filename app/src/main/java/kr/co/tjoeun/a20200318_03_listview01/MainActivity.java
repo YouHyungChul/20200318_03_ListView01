83e31a0438c53d0681a916779ad1664b3b326ffa
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-18 12:59:04 +0900

    목록 내용 변화 감지 함수 추가 실행

--- app/src/main/java/kr/co/tjoeun/a20200318_03_listview01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_03_listview01/MainActivity.java
@@ -1,13 +1,23 @@
 package kr.co.tjoeun.a20200318_03_listview01;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.tjoeun.a20200318_03_listview01.adapters.StoreAdapter;
 import kr.co.tjoeun.a20200318_03_listview01.databinding.ActivityMainBinding;
+import kr.co.tjoeun.a20200318_03_listview01.datas.Store;
 
 public class MainActivity extends BaseActivity {
+
+//    가게 목록이 담길 릿트
+    List<Store> storeDatas = new ArrayList<>();
+//    .......
+    StoreAdapter storeAdapter = null;
+
     ActivityMainBinding binding = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,19 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        addStores();
+
+//        어댑터를 객체화 (우리가 작성한 생성자 활용)
+        storeAdapter = new StoreAdapter(mContext, R.layout.store_list_item,storeDatas);
+//         리스트뷰와 어댑터를 연결
+        binding.storeListView.setAdapter(storeAdapter);
+    }
+
+    void addStores() {
+        storeDatas.add(new Store("삼겹더하기 본점", 4));
+        storeDatas.add(new Store("밥해주는 남자", 3));
+        storeAdapter.notifyDataSetChanged();
 
     }
 }
--- app/src/main/java/kr/co/tjoeun/a20200318_03_listview01/adapters/StoreAdapter.java
+++ app/src/main/java/kr/co/tjoeun/a20200318_03_listview01/adapters/StoreAdapter.java
@@ -17,13 +17,13 @@ import kr.co.tjoeun.a20200318_03_listview01.datas.Store;
 //빨간줄 생기면  Alt + Enter 누르면 => 에러 해결 제안나옴
 //밑에서 두번째인 context, resource, objects 파라미터가 있는걸로 골라야함.
 // 각각 어느 화면, 어떤 모양, 어떤목록을 의미함
-public class StoreAdaptere extends ArrayAdapter<Store> {
+public class StoreAdapter extends ArrayAdapter<Store> {
 
     Context mContext; // 화면과 연결하는 연결고리
     List<Store> mList; // 뿌려줄 데이터 목록록 담고있는 변수
     LayoutInflater inf; // xml -> 객체화 시켜주는 역활 클래스
 
-    public StoreAdaptere(@NonNull Context context, int resource, @NonNull List<Store> objects) {
+    public StoreAdapter(@NonNull Context context, int resource, @NonNull List<Store> objects) {
         super(context, resource, objects);
 
 //        생성자가 실행될대 세개의 변수를 채워줌.
